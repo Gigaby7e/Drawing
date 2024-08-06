15edae663871eed27acf7a110dfa78f2147a8163
Roman Skoromnyi <r.skoromnyi@pixode.com>
2024-08-06 20:01:20 +0300

    Cleanup

---
+++ Assets/Scripts/Drawing/DrawingEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Drawing
+{
+    public static class DrawingEvents
+    {
+        public static Action<Color> BrushColorChanged;
+        public static Action<float> BrushSizeChanged;
+    
+        public static Action SaveDrawingProgress;
+        public static Action LoadDrawingProgress;
+        public static Action ClearDrawingProgress;
+    }
+}
